281d742fe01a16fa2ef645ffdc8365ed7d95b370
lpseymour <lpseymour@miners.utep.edu>
2024-02-13 22:30:56 -0700

    Update casa_8.py

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,16 @@
+import pandas as pd
+
+filename = "big-mac-full-index.csv"
+df = pd.read_csv(filename)
+
+print(df.columns)
+
+#4. using iterrows()
+
+for i,r in df.iterrows():
+    print(r['name'], r['local_price'])
+
+#6. Using apply ()
+    
+print(df.apply(lambda row: row['GDP_bigmac'], axis=1))
 
